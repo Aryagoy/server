654d8fc879fd82dd0e80915d5a7983b4e92006f6
Aryagoy <aryagoyal1301@gmail.com>
2019-05-11 00:40:51 +0530

    adding run file

---
+++ server/views.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+import json
+
+def data(request):
+    if request.method== "POST":
+        json_data = json.loads(request.body.decode())
+        try:
+            course= json_data['course']
+            teamcode= json_data['teamcode']
+            return JsonResponse({'success':status.HTTP_200_CREATED})
+        except KeyError:
+            return response()->json(STATUS_CODE);
